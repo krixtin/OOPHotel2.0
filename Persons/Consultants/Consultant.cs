2182aa32992d54f4c724063d563ed2cc9e3518e3
PaulinaPorsmyr <paulina.porsmyr@hotmail.com>
2025-09-30 10:19:04 +0200

    Initial structure

---
+++ Persons/Consultants/Consultant.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPHotel2._0.Persons.Consultants
+{
+    internal class Consultant
+    {
+    }
+}
