143c4be93ce0e88c48160781c43937c07a3ad3a2
Kristin Ahnlund <kristin.ahnlund@gmail.com>
2025-09-30 13:23:36 +0200

    testing new objects in program

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Security.Cryptography;
 using OOPHotel2._0.Persons.Consultants;
+using OOPHotel2._0.Persons.Employees;
 
 namespace OOPHotel2._0
 {
@@ -7,16 +8,16 @@ namespace OOPHotel2._0
     {
         static void Main(string[] args)
         {
-            Consultant consultant1 = new Consultant
+            Consultant consultant1 = new Consultant();
             {
-                Name = "Eva Expert",
-                Age = 48,
-                EmployeeId = "C001",
-                StartDate = new DateOnly(2025, 04, 30),
-                Salary = 0,
-                HourlyRate = 1500,
-                ConsultingFirm = "Hotel Consultants corp."
-            };
+                Name = "Eva Expert";
+                Age = 48;
+                EmployeeId = "C001";
+                StartDate = new DateOnly(2025, 04, 30);
+                Salary = 0;
+                HourlyRate = 1500;
+                ConsultingFirm = "Hotel Consultants corp.";
+            }
 
             Console.WriteLine("Konsult:");
             consultant1.PrintInfo();
@@ -24,6 +25,14 @@ namespace OOPHotel2._0
             consultant1.GiveAdvice();
             Console.WriteLine($"Timlön: {consultant1.HourlyRate} kr");
             Console.WriteLine($"Konsultfirm: {consultant1.ConsultingFirm}");
+
+            DateOnly houskeeper1StartDate = new DateOnly(2025, 03, 20);
+
+            Houskeeping houskeeper1 = new Houskeeping("Pelle", 43, "E001", houskeeper1StartDate, 30000, "städare", "lokalvård");
+
+            houskeeper1.PrintInfo();
+            houskeeper1.Introduce();
+            houskeeper1.Work();
 
         }
     }
